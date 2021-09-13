96a0616c9c7b3b0518cf68a372f755cf652e0505
Creole Desktop <jignesh@7span.com>
2021-09-13 11:55:11 +0530

    first commit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,11 +6,9 @@ import rootSaga from '../sagas';
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
-
   return {
     ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
     runSaga: sagaMiddleware.run(rootSaga)
   }
 };
-
 export default configureStore;
